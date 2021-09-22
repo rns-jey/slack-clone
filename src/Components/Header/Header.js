fe6c92f567ee16fd087a76e30f11144b6ef7f1fa
JM Arenas <rns.jey@gmail.com>
2021-09-22 22:54:51 +0800

    Added Header Function Component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,3 +2,8 @@ import './Header.css';
 import { FaRegClock } from 'react-icons/fa';
 import { FaSearch } from 'react-icons/fa';
 import { FaRegQuestionCircle } from 'react-icons/fa';
+
+function Header() {
+  
+}
+
