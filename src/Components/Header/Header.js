8d8a969c8f051578994c4a3b7c5df6088c0ef905
JM Arenas <rns.jey@gmail.com>
2021-09-22 22:58:01 +0800

    Added header container

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,11 @@ import { FaSearch } from 'react-icons/fa';
 import { FaRegQuestionCircle } from 'react-icons/fa';
 
 function Header() {
-  
+  return (
+    <div className="header-container">
+
+    </div>
+  );
 }
 
 export default Header;
