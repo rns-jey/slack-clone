64eb57f65ec337d409a4d7b41ad0fe8f1d913a41
JM Arenas <rns.jey@gmail.com>
2021-09-22 22:51:05 +0800

    Added Mock page to view header component

---
+++ src/Pages/Mock.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from '../Components/Header/Header'
+
+function Mock() {
+  return (
+    <div className="Main">
+      <Header />
+    </div>
+  );
+}
+
+export default Mock;
