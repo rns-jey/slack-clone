74d984a1fc5ab1cf2741f944aae4428749f63d53
TanTanMa <gelatan12@gmail.com>
2021-09-23 20:18:07 +0800

    functional login logout

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,9 +1,46 @@
-import React from "react";
+import LoginForm from "../Components/Login/LoginForm";
+import React, {useState} from "react";
 
-function Login() {
+export default function LogUser(){
+  const adminUser = {
+      email:"admin@admin.a",
+      password: "admin123",  
+  }
+
+  const [user,SetUser] = useState({name:"", email:""});
+  const [error, SetError] = useState("");
+
+  function Login(details) {
+      console.log(details);
+      if (details.email == adminUser.email && details.password == adminUser.password){
+        console.log(`we in`)
+        SetUser({
+          name: details.name,
+          email: details.email
+        });
+      } else {
+        console.log(`details dont match`);
+        SetError("details dont match");
+      }
+  }
+
+  function Logout() {
+      SetUser({name:"", email:""});
+  }
+
   return (
-    <h1>Login</h1>
-  );
+      <div className="App">
+         {(user.email !="") ? (
+             <div className="welcome">
+                 <h2>Welcome, 
+                   <span>{user.name}</span>
+                </h2>
+                 <button onClick={Logout}>LogOut</button>
+             </div>
+         ) : (
+          <LoginForm Login={Login} error={error}/>
+         )}
+      </div>
+      );
 }
 
-export default Login;
