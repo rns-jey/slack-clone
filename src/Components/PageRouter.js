26c4114b80cb3da311517e9e84d3316a0c26583b
JM Arenas <rns.jey@gmail.com>
2021-09-25 07:11:13 +0800

    Added redirect function
    Redirect user to login when has not logged in before
    Redirect user to home when has logged in before

--- src/Components/PageRouter.js
+++ src/Components/PageRouter.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import Home from  '../Pages/Home';
 import Login from  '../Pages/Login';
 import CreateUser from "../Pages/CreateUser";
 
 function PageRoute() {
+  const [ isLogged, setLogin ] = useState(false);
+
+  useEffect(() => {
+    const rememberMe = localStorage.getItem('rememberMe') === 'true';
+    const user = rememberMe ? localStorage.getItem('user') : '';
+
+    setLogin(prevLog => (user.length > 0 ? true : false))
+  }, []);
+
+  console.log(isLogged)
+  
   return (
     <Router>
       <Switch>
         <Route exact path="/home" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={CreateUser} />
+        <Route exact path="/">
+          {isLogged ? <Redirect to="/home" /> : <Redirect to="/login" />}
+        </Route>
       </Switch>
     </Router>
   );
