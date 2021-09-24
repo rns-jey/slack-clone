f229b247c875c8b7800c550aa32ee6fc22c88d4c
annora <annora@LAPTOP-624LMDFD.localdomain>
2021-09-24 17:37:51 +0800

    added sidebar options no func yet

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Home from  './Pages/Home';
-import Login from  './Pages/Login';
 import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/login" component={Login} />
-      </Switch>
-    </Router>
-    <div className="App">
-      <Sidebar/>
-    </div>
+    <Sidebar />
   );
 }
 
