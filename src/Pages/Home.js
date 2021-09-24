967eb075e286ee6866e2784123b1ec761bbdc0d3
annora <annora@LAPTOP-624LMDFD.localdomain>
2021-09-24 22:17:25 +0800

    added new folder chat with chat.js and chat.css and imported in home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from '../Components/Header/Header'
 import Sidebar from '../Components/Sidebar/Sidebar';
+import Chat from '../Components/Chat/Chat';
 
 function Home() {
   return (
     <div className="Main">
       <Header />
-      <Sidebar/>
+      <Sidebar />
+      <Chat />
     </div>
   );
 }
