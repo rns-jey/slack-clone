fa1390b3a0dc1233b5a49f73b8f219f28e5b4db0
TanTanMa <gelatan12@gmail.com>
2021-09-25 10:26:23 +0800

    Changed page route of createuser to Register

--- src/Components/PageRouter.js
+++ src/Components/PageRouter.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from  '../Pages/Home';
 import Login from  '../Pages/Login';
 import Mock from '../Pages/Mock';
-import CreateUser from "../Pages/CreateUser";
+import Register from '../Pages/Register';
 
 function PageRoute() {
   return (
@@ -12,7 +12,7 @@ function PageRoute() {
         <Route exact path="/home" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/mock" component={Mock} />
-        <Route exact path="/register" component={CreateUser} />
+        <Route exact path="/register" component={Register} />
       </Switch>
     </Router>
   );
