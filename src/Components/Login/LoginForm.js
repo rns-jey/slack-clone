f4f0f5174ed1a085b335f5ea1533472573f85050
JM Arenas <rns.jey@gmail.com>
2021-09-25 09:10:57 +0800

    Built initial login form layout

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,6 +1,30 @@
 import React from 'react';
 
 export default function LoginForm(){
-
+  return (
+    <form>
+      <h2>Login</h2>
+      <div className="error"></div>
+      <div className="form-inner">
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input 
+            type="text" 
+            name="email" 
+            id="email"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input 
+            type="password" 
+            name="password"
+            id="password"
+          />
+        </div>
+        <input type="submit" value="LOGIN"/>
+      </div>
+    </form>
+  )
 };
 
