e2073d76c8e6e09bf4d5a3ca4ebfc48694dd3033
JM Arenas <rns.jey@gmail.com>
2021-09-25 09:28:44 +0800

    Added the redirect funtion in login page to prevent access if already has logged on

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import LoginForm from "../Components/Login/LoginForm";
+import RedirectToLogin from "../Components/Router/RedirectToLogin";
 
 export default function LogUser(){
   return (
     <>
+      <RedirectToLogin />
       <LoginForm />
       <div className="linktoCreateUser">
           <span>New to Slack? </span>
