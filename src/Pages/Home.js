4cadeb52a1aea48845e33362eada0d453b7028b2
JM Arenas <rns.jey@gmail.com>
2021-09-25 09:24:54 +0800

    Added the redirect funtion in home page to prevent access without logging in

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import RedirectToLogin from "../Components/Router/RedirectToLogin";
 import Header from '../Components/Header/Header'
 import Sidebar from '../Components/Sidebar/Sidebar';
 
 function Home() {
   return (
     <div className="Main">
+      <RedirectToLogin />
       <Header />
       <Sidebar/>
     </div>
