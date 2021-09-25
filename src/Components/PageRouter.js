a58002bad898afae0e116192fd2ab85e52bc1edf
JM Arenas <rns.jey@gmail.com>
2021-09-25 09:25:57 +0800

    Clean up code in pageroute component and added redirect to login function

--- src/Components/PageRouter.js
+++ src/Components/PageRouter.js
@@ -1,28 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import RedirectToLogin from "../Components/Router/RedirectToLogin";
 import Home from  '../Pages/Home';
 import Login from  '../Pages/Login';
 import CreateUser from "../Pages/CreateUser";
 
 function PageRoute() {
-  const [ isLogged, setLogin ] = useState(false);
-
-  useEffect(() => {
-    const rememberMe = localStorage.getItem('rememberMe') === 'true';
-    const user = rememberMe ? localStorage.getItem('user') : '';
-
-    setLogin(prevLog => (user.length > 0 ? true : false))
-  }, []);
-
   return (
     <Router>
       <Switch>
-        <Route exact path="/home" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={CreateUser} />
-        <Route exact path="/">
-          {isLogged ? <Redirect to="/home" /> : <Redirect to="/login" />}
-        </Route>
+        <Route exact path="/home" component={Home} />
+        <RedirectToLogin />
       </Switch>
     </Router>
   );
