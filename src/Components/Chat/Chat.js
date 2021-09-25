220f707264d7927ffc5b698ad50bd0d35b932d67
JM Arenas <rns.jey@gmail.com>
2021-09-25 11:53:27 +0800

    added new folder chat with chat.js and chat.css and imported in home.js

---
+++ src/Components/Chat/Chat.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react';
+import "./Chat.css";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+function Chat() {
+    return (
+        <Router>
+        <div className="chat_container">
+            <h1>Testing</h1>
+        </div>
+        </Router>
+    );
+}
+
+export default Chat; 
