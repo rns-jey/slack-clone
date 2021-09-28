1d6c905c968da9ec4e3cb69ef1bea62fc5b6aa04
JM Arenas <rns.jey@gmail.com>
2021-09-28 17:39:21 +0800

    Built initial new homepage layout

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import './App.css';
 
 function App() {
   return (
-    <> </>
+    <div className="Main">
+      <div className="header-container">
+
+      </div>
+      <div className="workspace">
+        <div className="sidebar">
+
+        </div>
+        <div className="chat-container">
+          <div className="chat-header"></div>
+          <div className="chat-body"></div>
+          <div className="chat-box"></div>
+        </div>
+      </div>
+    </div>
   );
 }
 
