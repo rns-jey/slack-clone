4d3ece01ddc85fa332936cfc5c7e165cfc00e62e
JM Arenas <rns.jey@gmail.com>
2021-09-29 14:36:16 +0800

    Rebuilt the workspace component

--- src/App.js
+++ src/App.js
@@ -1,66 +1,15 @@
 import './App.css';
 import React from "react";
 import Header from './Components/Header/Header';
-import CreateIcon from "@material-ui/icons/Create";
-import { Link, useLocation, Switch, Route, Redirect } from 'react-router-dom';
-
-function NavLink({ to, activeClassName, inactiveClassName, className, ...rest }) {
-  let location = useLocation();
-  let isActive = location.pathname === to;
-  let allClassNames = (isActive ? `${className} ${activeClassName}` : ` ${className}`)
-
-  return <Link className={allClassNames} to={to} {...rest} />;
-}
+import Workspace from './Workspace/Workspace';
 
 function App() {
   return (
     <div className="Main">
       <Header />
-      <div className="workspace">
-        <div className="sidebar">
-            <div className="sidebar_header">
-                <div className="sidebar_info">
-                    <h2>Avion School</h2>
-                </div>
-                <CreateIcon />
-            </div>
-            <div className="sidebar_options">
-                <NavLink activeClassName="sidebarOptionActive" inactiveClassName="sidebarOptionInActive" className="sidebarOption" to="/channel1">
-                  Channel1
-                </NavLink>
-                <NavLink activeClassName="sidebarOptionActive" inactiveClassName="sidebarOptionInActive" className="sidebarOption" to="/channel2">
-                  Channel2
-                </NavLink>
-                <NavLink activeClassName="sidebarOptionActive" inactiveClassName="sidebarOptionInActive" className="sidebarOption" to="/channel3">
-                  Channel3
-                </NavLink>
-            </div>
-        </div>
-
-        <Switch>
-          <Route path="/channel1">
-            <Page title="Channel1" />
-          </Route>
-          <Route path="/channel2">
-            <Page title="Channel2" />
-          </Route>
-          <Route path="/channel3">
-            <Page title="Channel3" />
-          </Route>
-        </Switch>
-      </div>
+      <Workspace />
     </div>
   );
-}
-
-function Page({ title }) {
-  return (
-    <div className="chat-container">
-      <div className="chat-header">{title}</div>
-      <div className="chat-body"></div>
-      <div className="chat-box"></div>
-    </div>
-  )
 }
 
 export default App;
