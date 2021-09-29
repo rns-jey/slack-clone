c2461fe9637bef977229da4032256259d7edf079
JM Arenas <rns.jey@gmail.com>
2021-09-29 14:30:06 +0800

    Rebuilt the header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import './App.css';
 import React from "react";
-import { FaRegClock } from 'react-icons/fa';
-import { FaSearch } from 'react-icons/fa';
-import { FaRegQuestionCircle } from 'react-icons/fa';
-import { BsPersonFill } from 'react-icons/bs';
+import Header from './Components/Header/Header';
 import CreateIcon from "@material-ui/icons/Create";
 import { Link, useLocation, Switch, Route, Redirect } from 'react-router-dom';
-
-const HeaderComponents = (props) => {
-  return <div className={props.title}>{props.children}</div>
-}
 
 function NavLink({ to, activeClassName, inactiveClassName, className, ...rest }) {
   let location = useLocation();
@@ -22,38 +15,7 @@ function NavLink({ to, activeClassName, inactiveClassName, className, ...rest })
 function App() {
   return (
     <div className="Main">
-      <div className="header-container">
-        <HeaderComponents title="left-header">
-          <FaRegClock />
-        </HeaderComponents>
-
-        <HeaderComponents title="mid-header">
-          <div>Search</div>
-          
-        </HeaderComponents>
-
-        <HeaderComponents title="right-header">
-          <FaRegQuestionCircle />
-        </HeaderComponents>
-
-        <HeaderComponents title="user-avi">
-          <BsPersonFill />
-          <div className="user-menu">
-            <div className="user-details">
-              <div className="user-image">
-                <BsPersonFill />
-              </div>
-              <div className="user-name-status">
-                <div className="user-name">jmarenas@gmail.com</div>
-                <div className="user-status">Active</div>
-              </div>
-            </div>
-            <div className="signout-container">
-              <span id="sign-out">Sign out</span>
-            </div>
-          </div>
-        </HeaderComponents>
-      </div>
+      <Header />
       <div className="workspace">
         <div className="sidebar">
             <div className="sidebar_header">
