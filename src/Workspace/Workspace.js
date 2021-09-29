1776a419fec253f3daa1ba2601f83f9215d136ff
JM Arenas <rns.jey@gmail.com>
2021-09-29 15:06:30 +0800

    Rebuilt Workspace component

--- src/Workspace/Workspace.js
+++ src/Workspace/Workspace.js
@@ -1,59 +1,12 @@
-
-import CreateIcon from "@material-ui/icons/Create";
-import { useLocation, Link, Switch, Route } from 'react-router-dom';
-
-function NavLink({ to, activeClassName, inactiveClassName, className, ...rest }) {
-  let location = useLocation();
-  let isActive = location.pathname === to;
-  let allClassNames = (isActive ? `${className} ${activeClassName}` : ` ${className}`)
-
-  return <Link className={allClassNames} to={to} {...rest} />;
-}
+import './Workspace.css'
+import SideNav from './SideNav';
+import ChatBody from './ChatBody';
 
 export default function Workspace() {
   return (
     <div className="workspace">
-      <div className="sidebar">
-          <div className="sidebar_header">
-              <div className="sidebar_info">
-                  <h2>Avion School</h2>
-              </div>
-              <CreateIcon />
-          </div>
-          <div className="sidebar_options">
-              <NavLink activeClassName="sidebarOptionActive" inactiveClassName="sidebarOptionInActive" className="sidebarOption" to="/channel1">
-                Channel1
-              </NavLink>
-              <NavLink activeClassName="sidebarOptionActive" inactiveClassName="sidebarOptionInActive" className="sidebarOption" to="/channel2">
-                Channel2
-              </NavLink>
-              <NavLink activeClassName="sidebarOptionActive" inactiveClassName="sidebarOptionInActive" className="sidebarOption" to="/channel3">
-                Channel3
-              </NavLink>
-          </div>
-      </div>
-
-      <Switch>
-        <Route path="/channel1">
-          <Page title="Channel1" />
-        </Route>
-        <Route path="/channel2">
-          <Page title="Channel2" />
-        </Route>
-        <Route path="/channel3">
-          <Page title="Channel3" />
-        </Route>
-      </Switch>
-    </div>
-  )
-}
-
-function Page({ title }) {
-  return (
-    <div className="chat-container">
-      <div className="chat-header">{title}</div>
-      <div className="chat-body"></div>
-      <div className="chat-box"></div>
+      <SideNav />
+      <ChatBody />
     </div>
   )
 }
