9d139c39170118ae4ab65b33b892823f0ca46ad6
JM Arenas <rns.jey@gmail.com>
2021-09-30 20:08:21 +0800

    Realigned conflict files to master

--- src/Components/Sidebar/SidebarOption.js
+++ src/Components/Sidebar/SidebarOption.js
@@ -13,23 +13,16 @@ function SidebarOption({Icon, title, id, addChannelOption}) {
     };
     const addChannel = () => {
     }
-    return ( 
-        <div 
-            className="sidebarOption" 
-            onClick={
-                addChannelOption ? 
-                addChannel : 
-                selectChannel}>
-                    {Icon && <Icon className="sidebarOption_icon"/>}
-                    {Icon ? 
-                        (<h3>{title}</h3>) : 
-                        (<h3 className="sidebarOption_channel">
-                            <span className="sidebarOption_hash">#</span>
-                            {title}
-                        </h3>)
-                    }
-        </div>
-    )
-}
+    return ( <div className="sidebarOption" onClick={addChannelOption ? addChannel : selectChannel}>
+        {Icon && <Icon className="sidebarOption_icon"/>}
+        {Icon ? 
+        (<h3>{title}</h3>) : 
+        (<h3 className="sidebarOption_channel">
+            <span className="sidebarOption_hash">#
+            </span>{title}</h3>)
+        }
+    </div>
+    );
+  }
 
 export default SidebarOption;
