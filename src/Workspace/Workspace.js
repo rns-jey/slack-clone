f47182ea5b0910f985a5e69f609482ba204dea45
JM Arenas <rns.jey@gmail.com>
2021-09-30 21:01:38 +0800

    Added useEffect module

--- src/Workspace/Workspace.js
+++ src/Workspace/Workspace.js
@@ -1,8 +1,14 @@
 import './Workspace.css'
 import SideNav from './SideNav';
 import ChatBody from './ChatBody';
+import React, { useEffect } from 'react';
 
 export default function Workspace() {
+
+  useEffect(() => {
+    
+  }, []);
+
   return (
     <div className="workspace">
       <SideNav />
