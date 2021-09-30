098d10da222b2014dfa8df053d55ed3cea80eb6f
JM Arenas <rns.jey@gmail.com>
2021-09-30 22:09:36 +0800

    Fixed a bug where recent users is still not working
    Added a function where routes will change dynamically between Channels and Recents
    based on the type of navlink mapped

--- src/Workspace/SideNavOpt.js
+++ src/Workspace/SideNavOpt.js
@@ -1,18 +1,18 @@
 import { useLocation, Link } from 'react-router-dom';
 
-function NavLink({ to, activeClassName, inactiveClassName, className, ...rest }) {
+function NavLink({ type, to, activeClassName, inactiveClassName, className, ...rest }) {
   let location = useLocation();
   let isActive = location.pathname === to;
   let allClassNames = (isActive ? `${className} ${activeClassName}` : ` ${className}`)
 
-  return <Link className={allClassNames} to={`/C${to}`} {...rest} />;
+  return (type === "Channel" ? <Link className={allClassNames} to={`/C${to}`} {...rest} /> : <Link className={allClassNames} to={`/D${to}`} {...rest} />);
 }
 
 export default function SideNavOpt( { Channels, Recents }) {
   return (
     <div className="sidebar_options">
       {Channels.map(({ id, name }) => (
-        <NavLink key={id} activeClassName="sidebarOptionActive" inactiveClassName="sidebarOptionInActive" className="sidebarOption" to={id}>
+        <NavLink key={id} type="Channel" activeClassName="sidebarOptionActive" inactiveClassName="sidebarOptionInActive" className="sidebarOption" to={id}>
           {name}
         </NavLink>
       ))}
