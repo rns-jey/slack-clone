865f97630fe469e2c2e1e8eaa9d6a7840c93f4f5
JM Arenas <rns.jey@gmail.com>
2021-09-30 22:05:13 +0800

    Added routing function to connect side nav and chat component for recent user chatted

--- src/Workspace/ChatBody.js
+++ src/Workspace/ChatBody.js
@@ -1,9 +1,8 @@
-
 import './ChatBody.css'
 import { Switch, Route, Redirect } from 'react-router-dom';
 import ChatMsg from './ChatMsg';
 
-export default function ChatBody( { Channels }) {
+export default function ChatBody( { Channels, Recents }) {
   return ( 
     <Switch>
       <Route path="/home">
@@ -12,6 +11,11 @@ export default function ChatBody( { Channels }) {
       {Channels.map(({ id, name }) => (
         <Route path={`/C${id}`}>
           <ChatMsg title={name} />
+        </Route>
+      ))}
+      {Recents.map(({ id, uid }) => (
+        <Route key={id} exact path={`/D${id}`}>
+          <Page type="User" title={uid} convoID={id} />
         </Route>
       ))}
       <Redirect from="/" to="/home" />
