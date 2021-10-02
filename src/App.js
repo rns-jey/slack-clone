9058093ca05314fffc06358fe52925c3015561bb
TanTanMa <gelatan12@gmail.com>
2021-10-02 14:00:02 +0800

    added pagerouter(redacted) para if need ko umalis ng workspace

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from "react";
 import Header from './Components/Header/Header';
 import Workspace from './Workspace/Workspace';
+// import PageRoute from './Components/Router/PageRouter'
 
 function App() {
   return (
+    // <PageRoute/>
     <div className="Main">
-      <Header />
+      <Header />  
       <Workspace />
     </div>
   );
