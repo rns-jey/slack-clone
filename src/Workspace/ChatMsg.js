955a38da411a679f7201571587de2d57fc11a597
TanTanMa <gelatan12@gmail.com>
2021-10-02 14:02:36 +0800

    converted header to 1-liner, reuse config.js for chatmsg

--- src/Workspace/ChatMsg.js
+++ src/Workspace/ChatMsg.js
@@ -7,18 +7,7 @@ import configAPI from '../Components/assets/config';
 export default function ChatMsg({ type, title, convoID }) {
   const [chat, setChat] = useState([])
   const [message, setMessage] = useState("")
-  const userExpiry = localStorage.getItem('expiry');
-  const userUID = localStorage.getItem('uid');
-  const userAt = localStorage.getItem('at');
-  const userClient = localStorage.getItem('client');
-  const config = {
-    headers : {
-      "access-token": `${userAt}`,
-      client: `${userClient}`,
-      expiry: `${userExpiry}`,
-      uid: `${userUID}`
-    }
-  }
+  const config = configAPI();
 
   function reverseChat(arr) {
     let chatMsgs = []
