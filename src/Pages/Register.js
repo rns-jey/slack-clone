6a97670ea31c31382beb02ab8cfe440bb5ba086e
JM Arenas <rns.jey@gmail.com>
2021-10-06 17:38:31 +0800

    Added redirect function if user has already logged in
    this is for when user is trying to access register component through address bar

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Bottomlinks from '../Components/assets/Bottomlinks';
 import FormSignUp from '../Components/CreateUser/FormSignUp';
-
+import { Redirect } from "react-router"
 
 
 export default function Register() {
+    const user = (localStorage.getItem('uid') ? localStorage.getItem('uid') : '')
 
     return (
         <>
-        <FormSignUp></FormSignUp>
-        <Bottomlinks/>
+            {
+                user.length > 0 ?
+                <Redirect to="/home" /> :
+                <>
+                    <FormSignUp />
+                    <Bottomlinks/>
+                </>
+            }
         </>
     )
 };
