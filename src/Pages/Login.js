2e3839b46f96ad32f28f9a5c5ff1e046ca64cf2a
JM Arenas <rns.jey@gmail.com>
2021-10-06 17:22:27 +0800

    Added redirect function if user has already logged in
    this is for when user is trying to access login form through address bar

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Bottomlinks from "../Components/assets/Bottomlinks";
 import LoginForm from "../Components/Login/LoginForm";
-import RedirectToLogin from "../Components/Router/RedirectToLogin";
+import { Redirect } from "react-router"
+
 export default function LogUser(){
+  const user = (localStorage.getItem('uid') ? localStorage.getItem('uid') : '')
+
   return (
     <>
-      <RedirectToLogin />
-      <LoginForm />
-      <Bottomlinks/>
+      {
+        user.length > 0 ?
+        <Redirect to="/home" /> :
+        <>
+          <LoginForm />
+          <Bottomlinks/>
+        </>
+      }
     </>
   )
 }
