12f16b4fa4406839b918b2cbcbe71f0941ae4720
JM Arenas <rns.jey@gmail.com>
2021-10-07 15:05:54 +0800

    Rebuilding app routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
 import React from "react";
+import { Redirect, Route, Switch } from 'react-router';
+import Register from './Pages/Register'
+import Login from './Pages/Login'
 import Home from './Pages/Home/Home';
-import PageRoute from './Components/Router/PageRouter';
 
 function App() {
+  const user = (localStorage.getItem('uid') ? localStorage.getItem('uid') : '')
+
   return (
     <div className="App">
-      <PageRoute />
+      <Switch>
+        <Route path="/register">
+          <Register User={user} />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+        { 
+          user.length > 0
+          ? <Home /> 
+          : <Redirect to="/login" />
+        }
+        </Route>
+      </Switch>
     </div>
   );
 }
