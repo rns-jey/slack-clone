d2ddb49b63488d376c19d272086aea46777f78f1
TanTanMa <gelatan12@gmail.com>
2021-10-11 21:27:47 +0800

    removed manual email input

--- src/Components/AddUsers/AddUsers.js
+++ src/Components/AddUsers/AddUsers.js
@@ -1,84 +1,132 @@
 import axios from 'axios';
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import configAPI from '../assets/config';
+import avatar from "../assets/avatar.png"
 
-export default function AddUsers({isAUModalopen, channelID, channelTitle}) {
-    const uEmailRef = useRef(null);
-    const [uEmails, setuEmails] = useState();
-    const [emailStats, setEmailStats] = useState({
-        success: [],
-        IDs:[],
-    })
+export default function AddUsers({ isAUModalopen, channelID, channelTitle }) {
+    // const uEmailRef = useRef(null);
+    // const [uEmails, setuEmails] = useState();
+    // const [emailStats, setEmailStats] = useState({
+    //     success: [],
+    //     IDs: [],
+    // })
+    const [errMsg, setErrMsg] = useState(null)
+    const [sucMsg, setSucMsg] = useState(null)
     const config = configAPI();
+    // const baseURLChannels = 'http://206.189.91.54//api/v1/channels';
+    const baseURLUsers = 'http://206.189.91.54//api/v1/users';
 
-    function handleUserInput(e) {
-        const uIDsRawInput = uEmailRef.current.value;
-        const uIDsArr = uIDsRawInput.split(';');
-        const uIDsArrTrim = uIDsArr.map(ids => ids.trim())
-        setuEmails(uIDsArrTrim);
-    };
+    // function handleUserInput(e) {
+    //     const uIDsRawInput = uEmailRef.current.value;
+    //     const uIDsArr = uIDsRawInput.split(';');
+    //     const uIDsArrTrim = uIDsArr.map(ids => ids.trim())
+    //     setuEmails(uIDsArrTrim);
+    // };
 
-    function getUserId(){
-        const baseURL = 'http://206.189.91.54//api/v1/users';
+    // function getUserId() {
+    //     const baseURL = 'http://206.189.91.54//api/v1/users';
 
+    //     axios
+    //         .get(baseURL, config)
+    //         .then((resp) => {
+    //             let array = resp.data.data;
+    //             uEmails.forEach(element => {
+    //                 array.find(({ email, id }) => {
+    //                     if (email == element) {
+    //                         setEmailStats(emailStats.IDs.push(id),
+    //                             emailStats.success.push(email))
+    //                     }
+    //                 })
+    //             });
+    //             // console.log(emailStats,'getuserID then')              
+    //         })
+    //         .catch((err) => {
+    //             console.log(err.data, 'getuserId catch')
+    //         })
+    // }
+
+    function userToChannel(id, email) {
+
+        const baseURL = 'http://206.189.91.54//api/v1/channel/add_member'
+        const data = {
+            "id": channelID,
+            "member_id": id,
+        }
         axios
-        .get(baseURL, config)
-        .then((resp) => {
-            let array = resp.data.data;
-            uEmails.forEach(element => {
-                array.find(({email,id})=>{
-                    if (email == element){
-                        setEmailStats(emailStats.IDs.push(id), 
-                        emailStats.success.push(email))}
-                })
-            }); 
-            // console.log(emailStats,'getuserID then')              
-                })
-        .catch((err) =>{
-            console.log(err.data, 'getuserId catch')
-        })
-        }
-    
-    function userToChannel() {
-        getUserId();
-        setTimeout(() => {
-            emailStats.IDs.map((eIds) =>{
-                const baseURL = 'http://206.189.91.54//api/v1/channel/add_member'
-                const data = {
-                    "id": channelID,
-                    "member_id": eIds
-                }
-                axios
-                .post(baseURL,data,config)
-                .then((resp) =>{
+            .post(baseURL, data, config)
+            .then((resp) => {
+                if ('errors' in resp.data) {
+                    const { errors: [msg] } = resp.data;
+                    setErrMsg(`${msg}`)
+                    setSucMsg(null)
+                } else {
                     console.log(resp.data, 'usertochannel then')
-                })
-                .catch((err)=>{
-                    console.log(err, 'usertochannel catch')
-                })
-        })
-        }, 3000);
-        }
+                    setSucMsg(`${email} added`)
+                    setErrMsg(null)
+                }
+            })
+            .catch((err) => {
+                console.log(err, 'usertochannel catch')
+            })
+    }
 
-    return(
-        <div className={`CCBg ${!isAUModalopen ? 'hide': 'show' }`}>
+    const [users, setUsers] = useState([]);
+    const [filteredUser, filterUser] = useState(users);
+    const handleSearch = (event) => {
+        setErrMsg(null)
+        setSucMsg(null)
+        let value = event.target.value.toLowerCase();
+        let result = [];
+        result = users.filter((data) => {
+            return data.email.search(value) != -1
+        });
+
+        filterUser(result);
+    }
+    useEffect(() => {
+        axios
+            .get(baseURLUsers, config)
+            .then((res) => {
+                setUsers(res.data.data)
+                filterUser(res.data.data);
+                // makeChecklist(res.data.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+    }, []);
+
+
+
+    return (
+        <div className={`CCBg ${!isAUModalopen ? 'hide' : 'show'}`}>
             <div className="CCContainer">
                 <div className="MTitle">
-                <button className="CCcloseBtn" onClick={() => isAUModalopen(false)}>X</button>
+                    <button className="CCcloseBtn" onClick={() => isAUModalopen(false)}>X</button>
                 </div>
-                <div className="MFG">
-                    <h3>{channelTitle}</h3>
-                <div className="MFG">
-                    <label className="inputLabel">
-                        Email:
-                    </label>
-                    <input type="text" className='MtextInput'
-                    ref={uEmailRef}
-                    onChange={handleUserInput}
-                    placeholder="Enter Email address"/>
+
+                {errMsg ? (<div className='error'>{errMsg}</div>) : null}
+                {sucMsg ? (<div className='success'>{sucMsg}</div>) : null}
+                <div className="searchListinputCont">
+                    <label className="form-label">Search users</label>
+                    <input type="text"
+                        className="form-input"
+                        placeholder="Search by name, role or team" onChange={(event) => handleSearch(event)}>
+                    </input>
                 </div>
+                <div className="searchListCont">
+                    {filteredUser.slice(0, 5).map(({ email, id }) => (
+                        <div className="usersList" id={id}
+                            onClick={() => userToChannel(id, email)}>
+                            <img src={avatar} className='listAvatar' id={`avatar ${id}`} />
+                            <div className="Email" id={`email ${id}`}>
+                                {email}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
                 <button onClick={userToChannel}>Add me</button>
-            </div>
             </div>
         </div>
     )
