2b5e1b39806102e70edb59e40557d7f74bd7e29a
JM Arenas <rns.jey@gmail.com>
2021-10-15 17:06:26 +0800

    Import text area autosize component

--- src/Components/Workspace/ChatInput.js
+++ src/Components/Workspace/ChatInput.js
@@ -1,9 +1,11 @@
+import TextareaAutosize from "react-textarea-autosize";
+
 export default function ChatInput() {
   return (
     <div className="chat-box-container">
       <div className="chat-box">
         <form>
-          
+          <TextareaAutosize />
         </form>
       </div>
     </div>
