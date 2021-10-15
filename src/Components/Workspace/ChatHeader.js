fa29c9df6a5d735401075d06548089b604efc263
JM Arenas <rns.jey@gmail.com>
2021-10-15 16:05:34 +0800

    Added isMounted state for cleanups

--- src/Components/Workspace/ChatHeader.js
+++ src/Components/Workspace/ChatHeader.js
@@ -3,7 +3,11 @@ import { useEffect } from "react"
 export default function ChatHeader({ id, type }) {
 
   useEffect(() => {
+    let isMounted = true;
     
+    return () => {
+      isMounted = false;
+    };
   })
 
   return (
