446854f1c8b26f27847866c27679be833285b654
JM Arenas <rns.jey@gmail.com>
2021-10-15 17:08:16 +0800

    Added message state. set and  render values onchange

--- src/Components/Workspace/ChatInput.js
+++ src/Components/Workspace/ChatInput.js
@@ -1,11 +1,16 @@
 import TextareaAutosize from "react-textarea-autosize";
 
 export default function ChatInput() {
+  const [message, setMessage] = useState("");
+
   return (
     <div className="chat-box-container">
       <div className="chat-box">
         <form>
-          <TextareaAutosize />
+          <TextareaAutosize
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
         </form>
       </div>
     </div>
