22ad965cc04143da74d7a1b38bce79f0951e32aa
JM Arenas <rns.jey@gmail.com>
2021-10-15 17:14:02 +0800

    Send message request on submit

--- src/Components/Workspace/ChatInput.js
+++ src/Components/Workspace/ChatInput.js
@@ -1,14 +1,31 @@
 import { useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
+import { sendMessage } from "../../API/API";
 
 export default function ChatInput() {
   const [message, setMessage] = useState("");
+
+  const chatConfig = {
+    receiverID: id,
+    receiverType: type,
+    body: message,
+    headers: {
+      token: localStorage.getItem("at"),
+      client: localStorage.getItem("client"),
+      expiry: localStorage.getItem("expiry"),
+      uid: localStorage.getItem("uid"),
+    },
+  };
 
   function handleKeyPress(e) {
     const { key } = e;
 
     if (key === "Enter") {
-      
+      sendMessage(chatConfig)
+        .then((res) => {
+          setMessage("");
+        })
+        .catch((err) => console.log("Error Sending Message: ", err));
     }
   }
 
