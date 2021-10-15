539e3119b838f064a307fe3f9bccef73e255b1da
JM Arenas <rns.jey@gmail.com>
2021-10-15 17:09:15 +0800

    Import use state

--- src/Components/Workspace/ChatInput.js
+++ src/Components/Workspace/ChatInput.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 
 export default function ChatInput() {
@@ -7,6 +8,7 @@ export default function ChatInput() {
     <div className="chat-box-container">
       <div className="chat-box">
         <form>
+          
           <TextareaAutosize
             onChange={(e) => setMessage(e.target.value)}
             value={message}
