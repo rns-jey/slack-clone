8a80ff5e4dae2b90861ecb5d00db569cb46c9d21
JM Arenas <rns.jey@gmail.com>
2021-10-15 16:57:52 +0800

    Retrieve message request on API

--- src/Components/Workspace/ChatContent.js
+++ src/Components/Workspace/ChatContent.js
@@ -1,9 +1,29 @@
 import { useEffect } from "react"
+import { getMessage } from "../../API/API";
 
 export default function ChatContent({ id, type }) {
 
+  const chatConfig = {
+    receiverID: id,
+    receiverType: type,
+    headers: {
+      token: localStorage.getItem("at"),
+      client: localStorage.getItem("client"),
+      expiry: localStorage.getItem("expiry"),
+      uid: localStorage.getItem("uid"),
+    },
+  };
+
   useEffect(() => {
     let isMounted = true;
+
+    getMessage(chatConfig)
+      .then((convo) => {
+        if (isMounted) {
+          
+        }
+      })
+      .catch((err) => console.log(err));
 
     return () => {
       isMounted = false;
