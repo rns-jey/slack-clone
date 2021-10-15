870f7b1b5edda845d0b5fc55e036b457b580a402
JM Arenas <rns.jey@gmail.com>
2021-10-15 16:02:41 +0800

    Import and render chat header in chat wrapper component

--- src/Components/Workspace/ChatWrapper.js
+++ src/Components/Workspace/ChatWrapper.js
@@ -1,10 +1,12 @@
 import { useParams } from "react-router";
+import ChatHeader from "./ChatHeader";
 
 export default function ChatWrapper() {
   const { type, id } = useParams();
-  
+
   return (
     <div className="chat-container">
+      <ChatHeader id={id} type={type} />
       <div className="chat-box-container">
         <div className="chat-box">
           <form>
