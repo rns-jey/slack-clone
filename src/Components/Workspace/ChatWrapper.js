74a2e2232b7d74e94deabe2c30dfcaa4335307bc
JM Arenas <rns.jey@gmail.com>
2021-10-15 15:55:06 +0800

    Built a chat wrapper to hold chat header, body and input component

---
+++ src/Components/Workspace/ChatWrapper.js
@@ -0,0 +1,13 @@
+export default function ChatWrapper() {
+  return (
+    <div className="chat-container">
+      <div className="chat-box-container">
+        <div className="chat-box">
+          <form>
+            
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
