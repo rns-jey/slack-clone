79da31cd9168272a3fb4caa58fa7f7283634bd6d
JM Arenas <rns.jey@gmail.com>
2021-10-15 16:54:27 +0800

    Added use effect with isMount clean up

--- src/Components/Workspace/ChatContent.js
+++ src/Components/Workspace/ChatContent.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react"
+
 export default function ChatContent({ id, type }) {
+
+  useEffect(() => {
+    let isMounted = true;
+
+    return () => {
+      isMounted = false;
+    };
+  });
+
   return (
     <div className="chat-body">
       
